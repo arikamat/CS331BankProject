e11751959079539e88dcc4dd0a557d5bd8a66af4
Ari Kamat <arijkamat@gmail.com>
2023-12-07 16:29:47 -0500

    Lengthen homepage to avoid awkward footer placement

--- pages/index.js
+++ pages/index.js
@@ -8,22 +8,24 @@ import Navbar from '../components/navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>D&A Bank</title>
-        <meta name="description" content="" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div>
+            <Head>
+                <title>D&A Bank</title>
+                <meta name="description" content="" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main>
-        <Navbar/>
-        <Homepage/>
-        <Footer/>
-      </main>
+            <main>
+                <Navbar />
+                <div className="relative flex flex-col items-center justify-center min-h-screen overflow-hidden">
+                    <Homepage />
+                </div>
+                <Footer />
+            </main>
 
-      
-      
-    </div>
-  )
+
+
+        </div>
+    )
 }
