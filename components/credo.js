16ed3138a0bc491afc60b52404123257fe1e72a2
dylantonthat <dylantonthat107@gmail.com>
2023-12-12 13:31:55 -0500

    Update images in Credo/Homdpage components

--- components/credo.js
+++ components/credo.js
@@ -1,14 +1,17 @@
+import Image from 'next/image';
 import Link from "next/link";
 
 function Credo() {
   return (
     <section class="text-gray-600 body-font">
       <div class="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
-        <img
+        <Image
           class="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded"
           alt="hero"
-          src="https://dummyimage.com/720x600"
-        ></img>
+          src="/img/home.jpg"
+          height={720}
+          width={600}
+        ></Image>
         <div class="text-center lg:w-2/3 w-full">
           <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
             Here at D&A, We Stand With You
